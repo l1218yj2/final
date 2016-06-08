4e4cee91fa3eb5fcd3109358bec644895ceeec19
l1218yj2 <l1218yj2@github.com>
2016-06-08 06:47:49 +0000

    메익마이그레이션

--- final/urls.py
+++ final/urls.py
@@ -7,7 +7,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^shop/', include('blog.urls', namespace='shop')),
+    url(r'^shop/', include('shop.urls', namespace='shop')),
     url(r'^$', lambda request: redirect('/shop/')),
 ]
 
