7fac605c6331fd8c65a9d51f13a0ca0f234ee4c9
l1218yj2 <l1218yj2@github.com>
2016-06-08 05:39:58 +0000

    accounts와 shop의 urls.py생성

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.contrib.auth.views import login, logout
+
+urlpatterns = [
+    url(r'^login/$', login, name='login'),
+    url(r'^logout/$', logout, name='logout'),
+]
