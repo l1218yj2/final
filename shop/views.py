5790838256204958f0d3f8ebf7e52776eb01a292
l1218yj2 <l1218yj2@github.com>
2016-06-08 05:47:58 +0000

    구현해야할 함수 정리, shop에 user추가

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Shop, Category, Review
 
-# Create your views here.
+def index(request):
+    pass
+
+def category_new(request):
+    pass
+
+def category_edit(request, pk):
+    pass
+
+def category_detail(request, pk):
+    pass
+
+def shop_new(request):
+    pass
+
+def shop_edit(request, pk):
+    pass
+
+def shop_detail(request, pk):
+    pass
+
+def review_new(reqeust):
+    pass
+
+def review_edit(request):
+    pass
+
+def category_delete(request, pk):
+    pass
+
+def shop_delete(request, pk):
+    pass
+
+def review_delete(request, pk):
+    pass
